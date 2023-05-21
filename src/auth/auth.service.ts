0ce2d65de22db2208beed8a4d83678580465bdc6
김영욱 <duddnr787@naver.com>
2023-05-21 17:22:05 +0900

    - bcrypt 비번 암호화 -

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,39 @@
+import {
+  ConflictException,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { CreateUserDto } from './dto/create-user.dto';
+import { User } from './user.entity';
+import { UserRepository } from './user.repository';
+import * as bcrypt from 'bcryptjs';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    @InjectRepository(User)
+    private userRepository: UserRepository,
+  ) {}
+
+  async signUp(createUserDto: CreateUserDto): Promise<void> {
+    const { username, password } = createUserDto;
+
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const user = this.userRepository.create({
+      username,
+      password: hashedPassword,
+    });
+    try {
+      await this.userRepository.save(user);
+    } catch (error) {
+      if (error.code === '23505') {
+        throw new ConflictException('Existing username');
+      } else {
+        throw new InternalServerErrorException();
+      }
+    }
+  }
+}
